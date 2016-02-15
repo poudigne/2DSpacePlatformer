7c8608938113ddb9cc4a98ae7adc38c5b2547dbb
poudigne <pierluc0414@live.ca>
2016-02-15 11:15:49 -0500

    end of chapter 1

---
+++ Assets/Scripts/PlayerStateController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStateController : MonoBehaviour {
+
+    public enum PlayerStates
+    {
+        idle,
+        left,
+        right,
+        jump,
+        landing,
+        falling,
+        kill,
+        resurrect
+    }
+
+    public delegate void playerStateHandler(PlayerStates newState);
+
+    public static event playerStateHandler onStateChange;
+
+    void LateUpdate()
+    { 
+        float horizontal = Input.GetAxis("Horizontal");
+        if (horizontal != 0f)
+        {
+            CallStateChange(horizontal < 0 ? PlayerStates.left : PlayerStates.right);
+        }
+        else
+        {
+            CallStateChange(PlayerStates.idle);
+        }
+    }
+
+    void CallStateChange(PlayerStates newState)
+    {
+        if (onStateChange != null)
+        {
+            onStateChange(newState);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerStateListener.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+public class PlayerStateListener : MonoBehaviour {
+
+    public float playerWalkSpeed = 3f;
+    public float playerJumpForceVertical = 500f;
+    public float playerJumpForceHorizontal = 250f;
+    public GameObject playerRespawnPoint = null;
+    public GameObject bulletPrefab = null;
+    public Transform bulletSpawnTransform;
+
+    private Animator playerAnimator = null;
+    private PlayerStateController.PlayerStates previousState = PlayerStateController.PlayerStates.idle;
+    private PlayerStateController.PlayerStates currentState = PlayerStateController.PlayerStates.idle;
+    private bool playerHasLanded = true;
+
+    void OnEnable()
+    {
+        PlayerStateController.onStateChange += onStateChange;
+    }
+
+    void OnDisable()
+    {
+        PlayerStateController.onStateChange -= onStateChange;
+    }
+
+
+    void Start()
+    {
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    void LateUpdate()
+    {
+        onStateCycle();
+    }
+
+    void onStateCycle()
+    {
+        Vector3 localScale = transform.localScale;
+
+        switch (currentState)
+        {
+            case PlayerStateController.PlayerStates.idle:
+                break;
+            case PlayerStateController.PlayerStates.left:
+                transform.Translate(new Vector3((playerWalkSpeed * -1.0f) * Time.deltaTime, 0.0f, 0.0f));
+                if (localScale.x > 0.0f)
+                {
+                    localScale.x *= -1.0f;
+                    transform.localScale = localScale;
+                }
+
+                break;
+            case PlayerStateController.PlayerStates.right:
+                transform.Translate(new Vector3(playerWalkSpeed * Time.deltaTime, 0.0f, 0.0f));
+                if (localScale.x < 0.0f)
+                {
+                    localScale.x *= -1.0f;
+                    transform.localScale = localScale;
+                }
+                break;
+        }
+    }
+
+    public void onStateChange(PlayerStateController.PlayerStates newState) 
+    {
+        if (newState == currentState)
+            return;
+
+        if (!checkForValidStatePair(newState))
+            return;
+
+        switch (newState)
+        {
+            case PlayerStateController.PlayerStates.idle:
+                playerAnimator.SetBool("Walking", false);
+                break;
+            case PlayerStateController.PlayerStates.left:
+            case PlayerStateController.PlayerStates.right:
+                playerAnimator.SetBool("Walking", true);
+                break;
+        }
+
+        currentState = newState;
+    }
+
+    bool checkForValidStatePair(PlayerStateController.PlayerStates newState)
+    {
+        bool returnVal = false;
+        switch (currentState)
+        {
+            case PlayerStateController.PlayerStates.idle:
+                returnVal = true;
+                break;
+            case PlayerStateController.PlayerStates.left:
+                returnVal = true;
+                break;
+            case PlayerStateController.PlayerStates.right:
+                returnVal = true;
+                break;
+        }
+        return returnVal;
+    }
+}
